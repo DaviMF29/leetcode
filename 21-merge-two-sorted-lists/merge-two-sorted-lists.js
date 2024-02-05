9573745fb625ef54e9d08d0ce07011553ab14bb2
Davi Miguel <89204832+DaviMF29@users.noreply.github.com>
2024-02-05 14:36:37 -0300

    Time: 65 ms (38.06%) | Memory: 51.4 MB (18.67%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -10,13 +10,30 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
 
-    if(!list1 || !list2) return list1 || list2
-    if(list1.val<list2.val){
-        return new ListNode(list1.val,mergeTwoLists(list1.next,list2))
+const mergeTwoLists = (list1, list2) => {
+    let ans = new ListNode(-1); 
+    let current = ans;
+
+    while (list1 || list2) {
+        if (!list1) {
+            current.next = list2
+            break
+        }
+        if (!list2) {
+            current.next = list1
+            break
+        }
+
+        if (list1.val < list2.val) {
+            current.next = list1
+            list1 = list1.next
+        } else { 
+            current.next = list2
+            list2 = list2.next
+        }
+        current = current.next
     }
-    return new ListNode(list2.val,mergeTwoLists(list1,list2.next))
-    
-   
-};
+    return ans.next
+
+}
